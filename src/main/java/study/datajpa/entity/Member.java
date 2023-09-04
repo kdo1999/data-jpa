f306a155d60a6f44544007536056f93a970e9b91
kdo_1999/mac <kdo_1999@naver.com>
2023-09-04 18:35:55 +0900

    프로젝트 생성 및 공통 인터페이스 기능 학습

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,40 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter @Setter
+@ToString(of = {"id", "username", "age"})
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    /**
+     * 연관관계 메서드
+     * */
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+}
